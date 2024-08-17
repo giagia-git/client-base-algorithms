48d03497d9cc497a3ff77b9df90f0d22e1a0dfa0
Phan Hoang Gia <gia@Phans-MacBook-Air.local>
2024-08-17 22:49:04 +0700

    redeploy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import {Custom} from "./component/Custom";
+import {useState} from "react";
 
 function App() {
+    const [token, setToken] = useState("");
+    function handleClick() {
+        setToken("");
+    }
   return (
     <div className="App">
-        <div className={"my-app"}>
-          <p>This is my-app</p>
+        <div>
+            <Custom />
+          <input type={"text"} value={token} onChange={event => setToken(event.target.value)}/>
+            <button type={"submit"} onClick={handleClick}>submit</button>
         </div>
     </div>
   );
---
+++ src/component/Custom.js
@@ -0,0 +1,8 @@
+
+export function Custom() {
+    return (
+        <div>
+            <h3>Hello World ReactJS</h3>
+        </div>
+    )
+}
